d256ed0703526b7da982d0e77ab90976312d73fd
Eugene Martseniuk <eugene.martseniuk@otakoyi.com>
2024-02-05 10:51:16 +0200

    fix: supabase initialization

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:example/config/providers.dart' as providers;
+import 'package:example/features/common/infrastructure/entities/environment.dart';
 import 'package:example/flavors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 /// Initializes services and controllers before the start of the application
 Future<ProviderContainer> bootstrap() async {
@@ -10,6 +15,13 @@ Future<ProviderContainer> bootstrap() async {
   final container = ProviderContainer(
     overrides: [], //supabaseProvider.overrideWithValue(Supabase.instance)
     observers: [if (F.appFlavor == Flavor.local) _Logger()],
+  );
+  final configFile = await rootBundle.loadString(F.envFileName);
+  final env = Environment.fromJson(json.decode(configFile) as Map<String, dynamic>);
+
+  await Supabase.initialize(
+    url: env.supabaseUrl,
+    anonKey: env.supabaseAnonKey,
   );
   await providers.initializeProviders(container);
   return container;
--- lib/config/providers.dart
+++ lib/config/providers.dart
@@ -1,10 +1,4 @@
-import 'dart:convert';
-
 import 'package:example/features/auth/application/auth_controller.dart';
-import 'package:example/features/common/infrastructure/entities/environment.dart';
-import 'package:example/flavors.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -24,22 +18,10 @@ FutureOr<SharedPreferences> sharedPreferences(SharedPreferencesRef ref) {
 
 /// Exposes [Supabase] instance
 
-@riverpod
-FutureOr<Supabase> supabase(SupabaseRef ref) async {
-  final configFile = await rootBundle.loadString(F.envFileName);
-  final env = Environment.fromJson(json.decode(configFile) as Map<String, dynamic>);
-
-  return Supabase.initialize(
-    url: env.supabaseUrl,
-    anonKey: env.supabaseAnonKey,
-    debug: kDebugMode,
-  );
-}
-
 /// Exposes [SupabaseClient] client
 @riverpod
 SupabaseClient supabaseClient(SupabaseClientRef ref) {
-  return ref.watch(supabaseProvider).valueOrNull!.client;
+  return Supabase.instance.client;
 }
 
 ///
@@ -55,7 +37,6 @@ SupabaseClient supabaseClient(SupabaseClientRef ref) {
 Future<void> initializeProviders(ProviderContainer container) async {
   /// Core
   await container.read(sharedPreferencesProvider.future);
-  await container.read(supabaseProvider.future);
 
   /// Auth
   container.read(authControllerProvider);
